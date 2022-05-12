5d03ea3519fbad026a618a0b9581bb0e5003dfcf
kundan mishra <mishrakundan073@gmail.com>
2022-05-12 12:44:41 +0530

    added deploy scipts and unit tests

--- contracts/RewardToken.sol
+++ contracts/RewardToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract RewardToken is ERC20 {
-  constructor(uint256 initialSupply) ERC20("Reward", "RWD") {
-    _mint(msg.sender, initialSupply);
+  constructor() ERC20("Reward", "RWD") {
+    _mint(msg.sender, 1000000 * 10**18);
   }
 }
